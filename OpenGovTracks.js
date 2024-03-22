bd24c5b4636ebc588b1084cff125549d2f62030d
AshutoshSingh00001 <ashutoshsingh10@icloud.com>
2024-03-22 14:01:15 +0530

    fix

--- OpenGovTracks.js
+++ OpenGovTracks.js
@@ -4,7 +4,7 @@ const { ApiPromise, WsProvider } = require('@polkadot/api')
 const CronJob = require("cron").CronJob;
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
